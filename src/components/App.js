269cc24a85651fce8dbdff99bcc28e450d86eacb
Rogério Moura <rogerioazapfy@gmail.com>
2019-02-27 22:54:45 -0300

    Finalizando projeto

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ import './style.css';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('League Of Legends');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items })
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   };
 
   onVideoSelect = (video) => {
